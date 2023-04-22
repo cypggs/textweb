759f785fb0c981b5e37a3ad24ef1e22a02cb0f3b
qcypggs <qcypggs@qq.com>
2023-04-23 00:01:26 +0800

    sdf

--- cd.sh
+++ cd.sh
@@ -1,5 +1,5 @@
 git pull
 docker build -t textweb .
 docker stop textweb
-docker rename --force textweb textweb.bak`date +%m-%d`
+docker rename textweb textweb.bak`date +%m-%d`
 docker run -dit --name=textweb -p 88:88 textweb
