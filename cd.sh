b78d9493b24568c7a3f62ae7848bb6c33769ac42
qcypggs <qcypggs@qq.com>
2023-06-09 17:07:48 +0800

    sdf

--- cd.sh
+++ cd.sh
@@ -3,5 +3,5 @@ docker build -t textweb .
 docker stop textweb
 docker rename textweb textweb.bak`date +%m-%d`
 #docker run -dit --name=textweb --network my_network -p 88:88 textweb
-docker run -dit --name textweb --network my_network -p 88:88 -v /home/opc/textweb/contents.db:/app/contents.db textweb
+docker run -dit --name textweb --network my_network -v /home/opc/textweb/contents.db:/app/contents.db textweb
 
