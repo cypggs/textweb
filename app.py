13b0620e14880bda0f0a7566eea9e88f035eb9ae
qcypggs <qcypggs@qq.com>
2023-06-11 13:56:16 +0800

    delete

--- app.py
+++ app.py
@@ -18,17 +18,23 @@ def save_content(content):
     with sqlite3.connect(DATABASE) as conn:
         conn.execute("INSERT INTO content (content, timestamp) VALUES (?, ?)",
                      (content, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
 def load_content(page):
     offset = (page-1) * 20
     with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.execute("SELECT content, timestamp FROM content ORDER BY timestamp DESC LIMIT 20 OFFSET ?", (offset,))
+        cursor = conn.execute("SELECT id, content, timestamp FROM content ORDER BY timestamp DESC LIMIT 20 OFFSET ?", (offset,))
         results = cursor.fetchall()
         return [
             {
-                "content": result[0],
-                "timestamp": result[1]
+                "id": result[0],
+                "content": result[1],
+                "timestamp": result[2]
             } for result in results
         ]
+
+def delete_content(content_id):
+    with sqlite3.connect(DATABASE) as conn:
+        conn.execute("DELETE FROM content WHERE id = ?", (content_id,))
 
 init_db()
 
@@ -48,6 +54,16 @@ def api_load():
     page = request.args.get('page', 1, type=int)
     contents = load_content(page)
     return jsonify(contents)
+
+@app.route('/api/delete', methods=['POST'])
+def api_delete():
+    data = request.get_json()
+    content_id = data.get('id', None)
+    if content_id:
+        delete_content(content_id)
+        return jsonify({'message': 'Deleted'})
+    else:
+        return jsonify({'message': 'Invalid ID'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=88)
