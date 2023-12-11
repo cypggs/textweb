03da9714d99e2ce433f620f95f4a2304d671431c
qcypggs <qcypggs@qq.com>
2023-12-12 00:33:06 +0800

    df

--- cd.sh
+++ cd.sh
@@ -3,5 +3,5 @@ docker build -t textweb .
 docker stop textweb
 docker rename textweb textweb.bak`date +%m-%d`
 #docker run -dit --name=textweb --network my_network -p 88:88 textweb
-docker run -dit --name textweb --network my_network --restart always -v /home/opc/textweb/contents.db:/app/contents.db textweb
+docker run -dit --name textweb --restart always -p 88:88 -v ./contents.db:/app/contents.db textweb
 
